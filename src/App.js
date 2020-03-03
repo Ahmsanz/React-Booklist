362b83e109b6cc4fc60e358d89a1af847e0b7721
Ahmsanz <adrian.h.m.sanz@gmail.com>
2020-03-03 19:05:15 +0100

    1.0 version of the app working; list and form up to the task

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+
+import BookContextProvider from './contexts/BooksContext';
+import Navbar from './components/Navbar';
+import BookList from './components/BookList';
+import NewBookForm from './components/BookForm';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BookContextProvider>
+        <Navbar />
+        <BookList />
+        <NewBookForm />
+      </BookContextProvider>
     </div>
   );
 }
---
+++ src/components/BookList.js
@@ -0,0 +1,20 @@
+import React, {useContext} from 'react';
+import {BookContext} from '../contexts/BooksContext';
+import BookDetails from './BookDetails';
+
+const BookList = () => {
+    const {books} = useContext(BookContext)
+    return books.length ? ( 
+        <div className="book-list">
+            { books.map( book => {
+                return (
+                    <BookDetails book={book} key={book.id}/>
+                )
+            })}
+        </div>
+     ) : (
+         <div className="empty"> No books to ReadableStream. Hello free time :) </div>
+     )
+}
+ 
+export default BookList;
